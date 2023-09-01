75041780fbe9559a3db5d0cfd7c7e2f78d09b7f7
yang <yang87924@gmail.com>
2023-09-02 01:39:56 +0800

    feat: photo update OK v32

--- controller/dbServe.js
+++ controller/dbServe.js
@@ -12,7 +12,7 @@ exports.insertWall = async (req, res) => {
       data.moment,
       data.label,
       data.color,
-      data.imgurl
+      data.imgurl,
     ])
     .then((result) => {
       res.send({
@@ -36,15 +36,88 @@ exports.insertFeedback = async (req, res) => {
 };
 
 //新增評論
-exports.insertFeedback = async (req, res) => {
+exports.insertComment = async (req, res) => {
   let data = req.body;
   //console.log(data)
   await db
-    .insertComments([data.wallId, data.userId, data.imgurl, data.moment])
+    .insertComment([
+      data.wallId,
+      data.userId,
+      data.imgurl,
+      data.moment,
+      data.name,
+    ])
     .then((result) => {
       res.send({
         code: 200,
         message: result,
       });
     });
+};
+
+//刪除牆，主表對應多條子表一起刪除
+exports.deleteWall = async (req, res) => {
+  let data = req.body;
+  // if(data.imgurl){
+  //   //如果地址存在，刪除對應圖片
+  //   Mkdir.deleteWall('data/'+data.imgurl)
+  // }
+  await db.deleteWall(data.id).then((result) => {
+    res.send({
+      code: 200,
+      message: result,
+    });
+  });
+};
+
+//刪除反饋
+exports.deleteFeedback = async (req, res) => {
+  let data = req.body;
+  await db.deleteFeedback(data.id).then((result) => {
+    res.send({
+      code: 200,
+      message: result,
+    });
+  });
+};
+
+//刪除評論
+exports.deleteComment = async (req, res) => {
+  let data = req.body;
+  await db.deleteComment(data.id).then((result) => {
+    res.send({
+      code: 200,
+      message: result,
+    });
+  });
+};
+
+//分業查詢wall並獲取讚數，舉報，撤銷數據
+exports.findWallPage = async (req, res) => {
+  let data = req.body;
+  await db.findWallPage(data.page,data.pagesize,data.type,data.label)
+  .then(async result=>{
+    for(let i=0;i<result.lenght;i++){
+      //查詢對應強的讚，舉報，撤銷數據
+      result[i].like=await db.feedbackCount(result[i].id,0);
+      result[i].report=await db.feedbackCount(result[i].id,1);
+      result[i].revoke=await db.feedbackCount(result[i].id,2);
+      result[i].islike=await db.likeCount(result[i].id,data.userId);
+      result[i].comcount=await db.CommentCount(result[i].id);
+    }
+    res.send({
+      code:200,
+      message:result,
+    })
+  })
+};
+//倒敘分頁查牆的評論
+exports.findCommentPage = async (req, res) => {
+  let data = req.body;
+  await db.findCommentPage(data.page,data.pagesize,data.id).then((result) => {
+    res.send({
+      code: 200,
+      message: result,
+    });
+  });
 };
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,51 @@ module.exports = function (app) {
     res.type("html"); // 設定回應的Content-Type為HTML
     res.render("test");
   });
+
   //新增wall數據
   app.post("/insertwall", (req, res) => {
     controller.insertWall(req, res);
   });
- 
+
+  //新增反饋
+  app.post("/insertfeedback", (req, res) => {
+    controller.insertFeedback(req, res);
+  });
+
+  //新增評論
+  app.post("/insertcomment", (req, res) => {
+    controller.insertComment(req, res);
+  });
+
+  //刪除牆
+  app.post("/deletewall", (req, res) => {
+    controller.deleteWall(req, res);
+  });
+
+  //刪除反饋
+  app.post("/deletefeedback", (req, res) => {
+    controller.deleteFeedback(req, res);
+  });
+
+  //刪除評論
+  app.post("/deletecomment", (req, res) => {
+    controller.deleteComment(req, res);
+  });
+
+  //分業查詢wall並獲取讚數，舉報，撤銷數據
+  app.post("/findwallpage", (req, res) => {
+    controller.findWallPage(req, res);
+  });
+  //倒敘分頁查牆的評論
+  app.post("/findcommentpage", (req, res) => {
+    controller.findCommentPage(req, res);
+  });
+  //用戶進入進行ip登記
+  app.post("/signip", (req, res) => {
+    var ip = req.ip;
+    res.send({
+      code: 200,
+      ip: ip,
+    });
+  });
 };
